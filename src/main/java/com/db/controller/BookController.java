ddd27ad16489008a872520d6d5ccd16f8881b515
vikasgabhane <vmgabhane.vg@gmail.com>
2021-01-20 19:38:22 +0530

    profile and delete

--- src/main/java/com/db/controller/BookController.java
+++ src/main/java/com/db/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.db.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -82,7 +85,7 @@ public class BookController {
 	
 	//get book by id rest api
 		@GetMapping("/books1/{userId}")
-		public List<Book>  getBookByUserId(@PathVariable int userId)
+		public List<Book>  getBookNotByUserId(@PathVariable int userId)
 		{
 			/*User user = new User();
 			//List<Book> book = new ArrayList<Book>();
@@ -97,5 +100,25 @@ public class BookController {
 			
 		}
 
+		//get book by id rest api
+		@GetMapping("/books2/{userId}")
+		public List<Book>  getBookByUserId(@PathVariable int userId)
+		{
+					
+			return bookRepository.findByUserId(userId);
+					
+		}
+		
+		// delete Book rest api
+		@DeleteMapping("/books/{bookId}")
+		public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable int bookId){
+			Book book = bookRepository.findById(bookId)
+					.orElseThrow(() -> new ResourceNotFoundException("Book not exist with id :" + bookId));
+			
+			bookRepository.delete(book);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			return ResponseEntity.ok(response);
+		}
 	
 }
